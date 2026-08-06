9e4743461730986efe254b5e112a7bd60c392a54
Andres Gonzalez <andfgonzalezmor@unal.edu.co>
2026-08-06 15:38:01 -0400

    ReadTheDocs: Modifying config.py file

--- Docs/source/conf.py
+++ Docs/source/conf.py
@@ -1,5 +1,7 @@
 # Configuration file for the Sphinx documentation builder.
 
+import os
+
 # -- Project information
 
 project = 'HYDROTEL'
@@ -11,11 +13,15 @@ version = '4.3.7'
 # -- General configuration
 
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosectionlabel",
+    "sphinx.ext.extlinks",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.todo",
+    "sphinx_codeautolink",
+    "sphinx_copybutton",
+    "nbsphinx",
 ]
 
 intersphinx_mapping = {
@@ -24,17 +30,49 @@ intersphinx_mapping = {
 }
 intersphinx_disabled_domains = ['std']
 
+#The suffix(es) of source filenames.
+# You can specify multiple suffix as a dictionary of suffix: filetype
+source_suffix = {".rst": "restructuredtext"}
+
+# The master toctree document.
+master_doc = "index"
+
+# General information about the project.
+project = "hydrotel"
+copyright = "2026, INRS"
+author = "Institut national de la recherche scientifique"
+
 templates_path = ['_templates']
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_rtd_theme'
-# html_static_path = ['_static']
-# html_logo = "_static/test_logo.png"
-# html_theme_options = {
-#     'logo_only': True,
-#     'display_version': False,
-# }
+html_theme = 'furo'
 
-# -- Options for EPUB output
-epub_show_urls = 'footnote'
+html_theme_options = {
+    "light_logo": "hydrotel_logo_light_rtd.png",
+    "dark_logo": "hydrotel_logo_transparent_rtd.png",
+}
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+if not os.path.exists("_static"):
+    os.makedirs("_static")
+html_static_path = ["_static", "logos"]
+
+html_sidebars = {
+    "**": [
+        "sidebar/scroll-start.html",
+        "sidebar/brand.html",
+        "sidebar/search.html",
+        "sidebar/navigation.html",
+        "sidebar/ethical-ads.html",
+        "sidebar/scroll-end.html",
+    ]
+}
+
+def setup(app):
+    app.add_css_file("style.css")
