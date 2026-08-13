15b114d98dbe958c92e8e428b2a1831445d4b460
Andres Gonzalez <andfgonzalezmor@unal.edu.co>
2026-08-13 13:40:38 -0400

    rtd: Test modifying config.py structure

--- Docs/source/conf.py
+++ Docs/source/conf.py
@@ -13,23 +13,25 @@ version = '4.3.7'
 # -- General configuration
 
 extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.autosectionlabel",
-    "sphinx.ext.extlinks",
-    "sphinx.ext.intersphinx",
-    "sphinx.ext.viewcode",
-    "sphinx.ext.todo",
-    "sphinx_codeautolink",
-    "sphinx_copybutton",
-    "nbsphinx",
+    # "sphinx.ext.autodoc",
+    # "sphinx.ext.autosectionlabel",
+    # "sphinx.ext.extlinks",
+    # "sphinx.ext.intersphinx",
+    # "sphinx.ext.viewcode",
+    # "sphinx.ext.todo",
+    # "sphinx_codeautolink",
+    # "sphinx_copybutton",
+    # "nbsphinx",
+    # "myst_parser",
     "sphinx_design",
+    "sphinx.ext.extlinks",
 ]
 
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
-}
-intersphinx_disabled_domains = ['std']
+# intersphinx_mapping = {
+#     'python': ('https://docs.python.org/3/', None),
+#     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+# }
+# intersphinx_disabled_domains = ['std']
 
 #The suffix(es) of source filenames.
 # You can specify multiple suffix as a dictionary of suffix: filetype
@@ -43,20 +45,22 @@ project = "hydrotel"
 copyright = "2026, INRS"
 author = "Institut national de la recherche scientifique"
 
-templates_path = ['_templates']
+# templates_path = ['_templates']
 
 # The name of the Pygments (syntax highlighting) style to use.
 # pygments_style = 'sphinx'
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_book_theme'
-
+html_theme = os.environ.get("SPHINX_THEME", "sphinx_book_theme")
+html_logo = 'logo/hydrotel_logo_transparent_rtd.png'
 html_theme_options = {
-    "light_logo": "hydrotel_logo_light_rtd.png",
-    "dark_logo": "hydrotel_logo_transparent_rtd.png",
+    "repository_url": "https://github.com/gonzmoaf/hydrotel.git",
+    "use_repository_button": True,
+    "use_edit_page_button": True,
+    "use_issues_button": True,
+    "repository_branch": "asus",
+    "path_to_docs": "Docs",
+    "homepage_in_toc": False,
 }
 
-
-# def setup(app):
-#     app.add_css_file("style.css")
